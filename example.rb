567eb51a6c53f255115e214fbc8e3e91125b9598
Dennis Yu <dennisyu319@gmail.com>
2013-02-20 11:44:53 -0500

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,4 +1,4 @@
-require File.join File.dirname(__FILE__), 'summary'
+require File.join File.dirname(__FILE__), 'desk-sailthru-summary'
 
 sendSummary("Dennis", "today", "dyu@sailthru.com", "Desk Daily Summary")
 #note that "Dennis" is case sensitive
